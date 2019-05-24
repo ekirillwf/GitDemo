4be487bed0e0ca5bb791987ea312a5897949103c
Kirill Elsufev <eysmen@mail.ru>
2019-05-24 21:31:43 +0700

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Кирилл Елсуфьев on 24/05/2019.
+//  Copyright © 2019 Кирилл Елсуфьев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
